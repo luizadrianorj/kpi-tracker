f9a905818e93ca8da2882914038368251c967316
luizadrianorj <luizadrianorj@gmail.com>
2026-04-03 12:58:52 -0300

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-const CACHE = 'kpi-v2';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
-self.addEventListener('fetch', e => {
-  if(e.request.url.includes('firestore') || e.request.url.includes('googleapis')) return;
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-});
